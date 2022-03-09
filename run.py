387d0ee4d29db45934c2e319c0f64a47c78b2104
AnnieNeilson <bnefnee@gmail.com>
2022-03-09 14:27:17 +0000

    Finish validation function to also check the response is an integer

--- run.py
+++ run.py
@@ -26,31 +26,33 @@ def welcome():
     print(instructions.MENU)
 
 
-def user_response():
+def user_response_int():
     """
     gets response from the user
     """
     response = input(">")
-    print(response)
     validate_response_int(response)
-    
-    
 
 
 def validate_response_int(response):
     """
     Inside the try the string value is converted into integers,
-    It raises ValueError if it can't convert into ints or if there are too many numbers
+    It raises ValueError if it can't convert into ints or if there are too 
+    many numbers
     """
     try:
+        [int(response)]
         if len(response) != 1:
-            raise ValueError(
-                f"Only one number is required, please try again.\n"
-            )
+            raise ValueError("Only one number is required")
+            user_response_int()
     except ValueError as e:
-        print(f"Invalid response: {e}")
+        print(f"Invalid response: {e}, please try again.\n")
+        user_response_int()
+    int_response = int(response)
+    return int_response
+
 
 
 welcome()
-user_response()
+user_response_int()
 
